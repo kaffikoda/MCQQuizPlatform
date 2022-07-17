355ac8b152db90280919ed42406402e899bf39b9
kaffikoda <nikhilkumar2k14@gmail.com>
2022-07-17 08:37:34 +0530

    changed the name os user registration page to signup and wrote code to get the type of requests

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
-
-@app.route('/user_registration', methods=['GET', 'POST'])
-def user_registration():
-    user_name = request.form.get('new_username')
-    user_emailid = request.form.get('new_email')
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
 
 
-    return render_template('user_registration.html')
+@app.route('/user_registration', methods=["GET", "POST"])
+def signup_page():
+    if request.method == "POST":
+        # user_name = request.form["new_username"]
+        # user_emailid = request.form["new_email"]
+
+        user_name = request.form.get("new_username")
+        user_emailid = request.form.get("new_email")
+
+        # print(user_name + " " + user_emailid)
+
+        return render_template('homepage.html')
+    else:
+        return render_template('signup_page.html')
 
 
 if __name__ == "__main__":
