68a2dbd0cdbe72ffa739c3a6844dc7661322c9a1
kaffikoda <nikhilkumar2k14@gmail.com>
2022-08-13 22:20:03 +0530

    added tracke modifications

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -19,6 +19,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # db.init_app(app)
     # db.app = app
